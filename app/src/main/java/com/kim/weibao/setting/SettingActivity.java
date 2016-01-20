8d05dcf0c4a7bd5c896795d84f98312215074a06
jinweiyang <jwy8645@163.com>
2016-01-20 20:59:28 +0800

    修改设置无法打开的bug

--- app/src/main/java/com/kim/weibao/setting/SettingActivity.java
+++ app/src/main/java/com/kim/weibao/setting/SettingActivity.java
@@ -6,6 +6,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.text.Editable;
 import android.text.TextWatcher;
+import android.util.Log;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
